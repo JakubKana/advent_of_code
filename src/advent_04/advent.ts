75cbceae3eeddbf80bb46dd3db409471228d92c5
Jakub Káňa <jakubkana@gmail.com>
2025-01-04 01:21:59 +0100

    Advent 04 part 1 solution

--- src/advent_04/advent.ts
+++ src/advent_04/advent.ts
@@ -5,24 +5,24 @@ import {dirname} from "../helpers.js";
 const inputLoader = new InputLoader(path.join(dirname, "advent_04", "input.txt"));
 
 enum Direction {
-    BOTTOM_RIGHT_TOP_LEFT = 1,
+    TOP_LEFT_BOTTOM_RIGHT = 1,
     BOTTOM_LEFT_TOP_RIGHT = 2,
 }
 
+function reverseInternalArrays(array: string[]) {
+    return array.map((row) => row.split("").reverse().join(""));
+}
+
 function getRowsArray(input: string, reversed: boolean = false) {
-    const rowsArray = input.split("\n");
-
+    let rowsArray = input.split("\n");
     if (reversed) {
-        for (let i = 0; i < rowsArray.length; i++) {
-            rowsArray[i] = rowsArray[i].split("").reverse().join("");
-        }
-
+        rowsArray = reverseInternalArrays(rowsArray);
     }
     return rowsArray;
 }
 
 function getColumnsArray(rowsArray: string[], reversed: boolean = false) {
-    const columnsArray: string[] = [];
+    let columnsArray: string[] = [];
     for (let i = 0; i < rowsArray.length; i++) {
         const row = rowsArray[i];
         let columnRow: string = "";
@@ -37,41 +37,165 @@ function getColumnsArray(rowsArray: string[], reversed: boolean = false) {
         columnsArray.push(columnRow);
     }
     if (reversed) {
-        for (let i = 0; i < columnsArray.length; i++) {
-            columnsArray[i] = columnsArray[i].split("").reverse().join("");
-        }
+        columnsArray = reverseInternalArrays(columnsArray);
     }
     return columnsArray;
 }
 
-function getXDiagonalsArray(rowsArray: string[], direction: Direction, reversed: boolean = false) {
+function getDiagonalsArray(rowsArray: string[], direction: Direction) {
+
     const diagonalsArray: string[] = [];
-    if(direction === Direction.BOTTOM_RIGHT_TOP_LEFT) {
-        for (let i = 0; i < rowsArray.length; i++) {
-            let diagonal = "";
-            for (let j = 0; j < rowsArray.length; j++) {
-                diagonal += rowsArray[i + j][j];
+    const columnLength = rowsArray[0].length;
+    const rowsLength = rowsArray.length; // 5
+
+    if (direction === Direction.TOP_LEFT_BOTTOM_RIGHT) {
+        const startPosition = {columnI: 0, rowI: 0};
+        const endPosition = {columnI: 0, rowI: 0};
+        let goOn = true;
+        while (goOn) {
+            let currentPosition = {...startPosition};
+
+            let diagonalString = "";
+
+            if (currentPosition.columnI === endPosition.columnI && currentPosition.rowI === endPosition.rowI) {
+                diagonalString += rowsArray[currentPosition.rowI][currentPosition.columnI];
+            } else {
+                if (currentPosition.columnI < columnLength && currentPosition.rowI < rowsLength) {
+                    while (currentPosition.columnI <= endPosition.columnI && currentPosition.rowI >= endPosition.rowI && currentPosition.columnI <= columnLength && currentPosition.rowI <= rowsLength) {
+
+                        diagonalString += rowsArray[currentPosition.rowI][currentPosition.columnI];
+                        currentPosition.columnI++;
+                        currentPosition.rowI--;
+                    }
+                }
+
             }
-            diagonalsArray.push(diagonal);
+            diagonalsArray.push(diagonalString);
+
+            if (startPosition.columnI === columnLength - 1 && startPosition.rowI === rowsLength - 1) {
+
+                goOn = false;
+            }
+
+
+            if (startPosition.rowI < rowsLength - 1) {
+                startPosition.rowI++;
+            } else {
+                startPosition.columnI++;
+            }
+            if (endPosition.columnI < columnLength - 1) {
+                endPosition.columnI++;
+            } else {
+                endPosition.rowI++;
+            }
+
+
+        }
+    }
+    if (direction === Direction.BOTTOM_LEFT_TOP_RIGHT) {
+        const startPosition = {columnI: 0, rowI: rowsLength - 1};
+        const endPosition = {columnI: 0, rowI: rowsLength - 1};
+
+        let goOn = true;
+        while (goOn) {
+            let currentPosition = {...startPosition};
+
+            let diagonalString = "";
+
+            if (currentPosition.columnI === endPosition.columnI && currentPosition.rowI === endPosition.rowI) {
+                diagonalString += rowsArray[currentPosition.rowI][currentPosition.columnI];
+            } else {
+                if (currentPosition.columnI < columnLength && currentPosition.rowI < rowsLength) {
+                    while (currentPosition.columnI <= endPosition.columnI && currentPosition.rowI <= endPosition.rowI && currentPosition.columnI <= columnLength - 1 && currentPosition.rowI <= rowsLength - 1) {
+
+                        diagonalString += rowsArray[currentPosition.rowI][currentPosition.columnI];
+                        currentPosition.columnI++;
+                        currentPosition.rowI++;
+                    }
+                }
+
+            }
+            diagonalsArray.push(diagonalString);
+
+            if (startPosition.columnI === columnLength - 1 && startPosition.rowI === 0) {
+
+                goOn = false;
+            }
+
+            if (startPosition.rowI > 0) {
+                startPosition.rowI--;
+            } else {
+                startPosition.columnI++;
+            }
+            if (endPosition.columnI < columnLength) {
+                endPosition.columnI++;
+            } else {
+                endPosition.rowI--;
+            }
         }
     }
     return diagonalsArray;
 }
 
-function getYDiagonalsArray(rowsArray: string[], direction: Direction, reversed: boolean = false) {
-    const diagonalsArray: string[] = [];
 
-    return diagonalsArray;
+function getXmasCount(rowsArray: string[]) {
+    const regexpXMAS = /XMAS/g;
+    const regexpSAMX = /SAMX/g;
+    let xmasCount = 0;
+    for (const string of rowsArray) {
+        const result = [...string.matchAll(regexpXMAS)];
+        xmasCount += result.length;
+    }
+    for (const string of rowsArray) {
+        const result = [...string.matchAll(regexpSAMX)];
+        xmasCount += result.length;
+    }
+    return xmasCount;
 }
 
+
 function runPart1() {
+
     const input = inputLoader.loadInput();
     const rowsArray = getRowsArray(input);
-    const rowsArrayReversed = getRowsArray(input, true);
+    // const rowsArrayReversed = getRowsArray(input, true);
     const columnsArray = getColumnsArray(rowsArray);
-    const columnsArrayReversed = getColumnsArray(rowsArray, true);
+    // const columnsArrayReversed = getColumnsArray(rowsArray, true);
+    console.log({rowsArrayLength: rowsArray.length, columnsArrayLength: columnsArray[0].length});
 
+    // Diagonals
+    const diagonalArrayTopLeft = getDiagonalsArray(rowsArray, Direction.TOP_LEFT_BOTTOM_RIGHT);
+    const diagonalArrayBottomLeft = getDiagonalsArray(rowsArray, Direction.BOTTOM_LEFT_TOP_RIGHT);
+    const diagonalArrayTopLeftReversed = reverseInternalArrays(diagonalArrayTopLeft);
+    const diagonalArrayBottomLeftReversed = reverseInternalArrays(diagonalArrayBottomLeft)
+    console.log({
+        diagonalArrayTopLeft,
+        diagonalArrayBottomLeft,
+        diagonalArrayTopLeftReversed,
+        diagonalArrayBottomLeftReversed
+    });
 
+    // COUNTING OF XMAS OCCURENCES
+    let xmasCountRowsArray = getXmasCount(rowsArray);
+    let xmasCountColumnsArray = getXmasCount(columnsArray);
+
+    let xmasCountDiagonalArrayTopLeft = getXmasCount(diagonalArrayTopLeft);
+    let xmasCountDiagonalArrayBottomLeft = getXmasCount(diagonalArrayBottomLeft);
+
+    let xmasCountDiagonalArrayTopLeftReversed = getXmasCount(diagonalArrayTopLeftReversed);
+    let xmasCountDiagonalArrayBottomLeftReversed = getXmasCount(diagonalArrayBottomLeftReversed);
+
+    let total = xmasCountRowsArray + xmasCountColumnsArray + xmasCountDiagonalArrayTopLeft + xmasCountDiagonalArrayBottomLeft;
+
+    console.log({
+        xmasCountRowsArray,
+        xmasCountColumnsArray,
+        xmasCountDiagonalArrayBottomLeft,
+        xmasCountDiagonalArrayTopLeft,
+        xmasCountDiagonalArrayTopLeftReversed,
+        xmasCountDiagonalArrayBottomLeftReversed,
+        total
+    });
 
 }
 
