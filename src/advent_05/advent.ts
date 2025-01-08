fe8b653c991ef65c914e14ebe494f000fcf6634d
Jakub Káňa <jakubkana@gmail.com>
2025-01-08 01:24:06 +0100

    Sync

--- src/advent_05/advent.ts
+++ src/advent_05/advent.ts
@@ -2,18 +2,19 @@ import InputLoader from "../utils/Input.js";
 import path from "node:path";
 import {dirname} from "../helpers.js";
 
-const inputLoader = new InputLoader(path.join(dirname, "advent_05", "input_01.txt"));
+const inputLoaderPart1 = new InputLoader(path.join(dirname, "advent_05", "input_01.txt"));
+const inputLoaderPart2 = new InputLoader(path.join(dirname, "advent_05", "input_02.txt"));
 
 
 function runPart1() {
-    const input = inputLoader.loadInput();
+    const input = inputLoaderPart1.loadInput();
 
     console.log(input);
 }
 
 
 function runPart2() {
-    const input = inputLoader.loadInput();
+    const input = inputLoaderPart2.loadInput();
 
     console.log(input);
 }
