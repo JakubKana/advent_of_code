d74bbc17686be5003711e2cc15adfa69b3b4d278
Jakub Káňa <jakubkana@gmail.com>
2025-02-16 02:13:55 +0100

    dist delete

--- dist/helpers.js
+++
@@ -1,6 +0,0 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
-export { filename, dirname };
-//# sourceMappingURL=helpers.js.map
